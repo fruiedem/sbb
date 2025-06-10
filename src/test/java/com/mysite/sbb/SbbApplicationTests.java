2b366ce0c4ddbe41d271f361afa1db30027e80a8
fruiedem <fruiedem@gmail.com>
2025-06-10 16:25:22 +0900

    패키지 분리 및 문자열 반환 api 추가

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,25 +4,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
+
 @SpringBootTest
 class SbbApplicationTests {
+
 	@Autowired
-	private QuestionRepository questionRepository;
+	private AnswerRepository answerRepository;
 
 	@Test
 	void contextLoads() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1); // 첫번째 질문 저장
-//		List<Question> all = this.questionRepository.findAll();
-//		assertEquals(0, all.size());
+		List<Answer> all = this.answerRepository.findAll();
+		assertEquals(3, all.size());
 	}
 
 }
