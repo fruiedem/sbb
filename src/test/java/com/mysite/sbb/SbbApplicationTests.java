f147b5a769cca5c8cfd989dc129ec9c963e75e5d
fruiedem <fruiedem@gmail.com>
2025-06-10 15:15:38 +0900

    Initial commit

---
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbApplicationTests {
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Test
+	void contextLoads() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1); // 첫번째 질문 저장
+//		List<Question> all = this.questionRepository.findAll();
+//		assertEquals(0, all.size());
+	}
+
+}
